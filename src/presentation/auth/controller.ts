efc19eff3eedf6cff991fd2e61ad303812092915
carlos060798 <daniloangaritagarcia@gmail.com>
2023-11-20 15:45:11 -0500

    version 1.2.4

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,50 +1,50 @@
-import { Response,Request } from "express";
+import { Response, Request } from "express";
 import { RegisteruserDto } from '../../domain/dtos/auth/register-user-dto';
 import { AuthService } from '../../presentation/services/auth.service';
 import { CustomError, LoginuserDto } from '../../domain';
 
 
-export  class  AuthController{
+export class AuthController {
     constructor(
-        public readonly authService:AuthService
-    ) {}
+        public readonly authService: AuthService
+    ) { }
 
-   private handleError(error:unknown,res:Response){
-     if(error instanceof CustomError){
-         res.status(error.statusCode).json({error:error.message})     
+    private handleError(error: unknown, res: Response) {
+        if (error instanceof CustomError) {
+            res.status(error.statusCode).json({ error: error.message })
         }
-      console.log(`${error}`)
-      return res.status(500).json({error:"Error interno"})
+        console.log(`${error}`)
+        return res.status(500).json({ error: "Error interno" })
 
-   }
+    }
 
 
-   registerUser=(req:Request,res:Response)=>{
-     const [error,registerDto]=  RegisteruserDto.create(req.body)
-     if(error) return res.status(400).json(error)
-     
+    registerUser = (req: Request, res: Response) => {
+        const [error, registerDto] = RegisteruserDto.create(req.body)
+        if (error) return res.status(400).json(error)
 
-        this.authService.registerUser(registerDto!).then((user)=>{
+
+        this.authService.registerUser(registerDto!).then((user) => {
             res.status(200).json(user)
-        }).catch((error)=>{
-            this.handleError(error,res)
+        }).catch((error) => {
+            this.handleError(error, res)
         }
         )
-   }
-    loginUser= (req:Request,res:Response)=>{
-        const [error,loginDto]=  LoginuserDto.create(req.body)
-        if(error) return res.status(400).json(error)
-        
-         this.authService.loginUser(loginDto!)
-         .then((user)=> res.json(user))
-         .catch((error)=>{ this.handleError(error,res)})
+    }
+    loginUser = (req: Request, res: Response) => {
+        const [error, loginDto] = LoginuserDto.create(req.body)
+        if (error) return res.status(400).json(error)
+
+        this.authService.loginUser(loginDto!)
+            .then((user) => res.json(user))
+            .catch((error) => { this.handleError(error, res) })
 
 
 
     }
-    validateEmail=(req:Request,res:Response)=>{
-      const {token}= req.params
-        this.authService.validateEmailToken(token).then(()=>{ res.json({message:"Email validado"})}).catch((error)=>{ this.handleError(error,res)})
+    validateEmail = (req: Request, res: Response) => {
+        const { token } = req.params
+        this.authService.validateEmailToken(token).then(() => { res.json({ message: "Email validado" }) }).catch((error) => { this.handleError(error, res) })
     }
 
 }
